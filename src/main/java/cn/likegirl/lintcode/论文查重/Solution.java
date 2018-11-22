a9dfe77910498dc86ca9c6ff4a0304f87c78ae08
ke.chen <likegirl.cn@gmail.com>
2018-11-22 09:24:04 +0800

    2018112292406

---
+++ src/main/java/cn/likegirl/lintcode/论文查重/Solution.java
@@ -0,0 +1,118 @@
+package cn.likegirl.lintcode.论文查重;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.checkerframework.checker.units.qual.A;
+import sun.java2d.pipe.AAShapePipe;
+
+import java.util.*;
+
+/**
+ * 1463. 论文查重
+ * 我们定义，两个论文的相似度为最长的相似单词子序列 * 2 除以两篇论文的总长度。
+ * 给定两篇论文words1，words2（每个表示为字符串数组），和相似单词对列表pairs，求两篇论文的相似度。
+ * <p>
+ * 注意：相似关系是可传递的。例如，如果“great”和“good”类似，而“find”和“good”类似，那么“geat”和“find”类似。
+ * 相似性也是对称的。 例如，“great”和“good”相似，则“good”和“great”相似。
+ * 另外，一个词总是与其本身相似。
+ * <p>
+ * 样例
+ * 给出words1= ["great","acting","skills","life"] ，words2= ["fine","drama","talent","health"] ，pairs= [["great", "good"], ["fine", "good"], ["acting","drama"], ["skills","talent"]]，返回0.75。
+ * <p>
+ * `解释：
+ * 两篇单词相似的子单词序列为
+ * "great","acting","skills"
+ * "fine","drama","talent"
+ * 总长度为8
+ * 相似度为6/8=0.75`
+ * 给出words1= ["I","love","you"] ，words2= ["you","love","me"] ，pairs= [["I", "me"]]，返回0.33。
+ * <p>
+ * 解释：
+ * 两篇单词相似的子单词序列为
+ * "I"
+ * "me"
+ * 或
+ * "love"
+ * "love"
+ * 或
+ * "you"
+ * "you"
+ * 总长度为6
+ * 相似度为2/6=0.33
+ * 注意事项
+ * 两篇论文的长度都不超过800800，相似单词对数不超过30003000
+ */
+public class Solution {
+    /**
+     * @param words1: the words in paper1
+     * @param words2: the words in paper2
+     * @param pairs:  the similar words pair
+     * @return: the similarity of the two papers
+     */
+    public static float getSimilarity(List<String> words1, List<String> words2, List<List<String>> pairs) {
+        // Write your code here
+        List<List<String>> newPairs = new ArrayList<>();
+        List<String> pair;
+        List<String> item;
+        Set<String> similarityWords = new HashSet<>();
+        int count = 0;
+        for (int i = 0; i < pairs.size(); i++) {
+            pair = pairs.get(i);
+            for (int j = i + 1; j < pairs.size(); j++) {
+                item = pairs.get(j);
+                List<String> newPair = new ArrayList<>();
+                if (pair.get(0).equals(item.get(0))) {
+                    newPair.add(pair.get(1));
+                    newPair.add(item.get(1));
+                }
+                if (pair.get(0).equals(item.get(1))) {
+                    newPair.add(pair.get(1));
+                    newPair.add(item.get(0));
+                }
+                if (pair.get(1).equals(item.get(0))) {
+                    newPair.add(pair.get(0));
+                    newPair.add(item.get(1));
+                }
+                if (pair.get(1).equals(item.get(1))) {
+                    newPair.add(pair.get(0));
+                    newPair.add(item.get(0));
+                }
+                if (newPair.size() > 0) {
+                    newPairs.add(newPair);
+                }
+            }
+        }
+        pairs.addAll(newPairs);
+        for (String w1 : words1) {
+            similarityWords.clear();
+            for (List<String> p : pairs) {
+                if (p.get(0).equals(w1)) {
+                    similarityWords.add(p.get(1));
+                }
+                if (p.get(1).equals(w1)) {
+                    similarityWords.add(p.get(0));
+                }
+            }
+            for (String w2 : words2) {
+                if (similarityWords.contains(w2)) {
+                    count++;
+                }
+            }
+        }
+        return (float) (Math.round((count * 2F) / (words1.size() + words2.size()) * 100) / 100.0);
+    }
+
+    public static void main(String[] args) {
+        List<String> word1 = Arrays.asList("great","acting","skills","life");
+        List<String> word2 = Arrays.asList("fine","drama","talent","talent");
+        List<String> p1 = Arrays.asList("great", "good");
+        List<String> p2 = Arrays.asList("fine", "good");
+        List<String> p3 = Arrays.asList("acting", "drama");
+        List<String> p4 = Arrays.asList("skills", "talent");
+        List<List<String>> pairs = new ArrayList<>();
+        pairs.add(p1);
+        pairs.add(p2);
+        pairs.add(p3);
+        pairs.add(p4);
+        System.out.println(getSimilarity(word1, word2, pairs));
+    }
+}
--- src/main/java/cn/likegirl/lintcode/询问冷却时间/Solution.java
+++ src/main/java/cn/likegirl/lintcode/询问冷却时间/Solution.java
@@ -3,16 +3,16 @@ package cn.likegirl.lintcode.询问冷却时间;
 /**
  * 1467. 询问冷却时间
  * 你有一串的技能需要释放，释放顺序为arr。必须按照顺序释放。每个技能都有长度为n的冷却时间。也就是说，两个同类技能之间至少要间隔n秒。释放每个技能需要1秒,返回放完所有技能所需要的时间。
- *
+ * <p>
  * 样例
  * Given arr=[1,1,2,2],n=2.Return 8.
- *
+ * <p>
  * The order is [1, _, _, 1, 2, _, _, 2].So return 8.
  * Skill 1 is released in the 1st second, in the 2nd second and the 3rd second enters the cooling time, and the 4th second releases the second time.
  * Skill 2 is released in the 5th second, in the 6th second and the 7th second enters the cooling time, and the 8th second releases the second time.
- *
+ * <p>
  * Given arr=[1,2,1,2], n=2. Return 5.
- *
+ * <p>
  * The order is [1, 2, _, 1, 2].So return  5.
  * Skill 1 is released in the 1st second, in the 2nd second and the 3rd second enters the cooling time, and the 4th second releases the second time.
  * Skill 2 is released in the 2nd second, in the 3rd second and the 4th second enters the cooling time, and the 5th second releases the second time.
@@ -24,16 +24,36 @@ package cn.likegirl.lintcode.询问冷却时间;
 public class Solution {
     /**
      * @param arr: The release order
-     * @param n: The cooldown
+     * @param n:   The cooldown
      * @return: Return the time
      */
-    public int askForCoolingTime(int[] arr, int n) {
+    public static int askForCoolingTime(int[] arr, int n) {
         // Write your code here
+        int count = 0;
+        int[] currentWait = new int[101];
+        for (int i = 0; i < arr.length; i++) {
+//            if (currentWait[arr[i]] == 0) {
+//                count++;
+//                drop(currentWait, 1);
+//            } else {
+                count += 1 + currentWait[arr[i]];
+                drop(currentWait, 1 + currentWait[arr[i]]);
+//            }
+            currentWait[arr[i]] = n;
+        }
+        return count;
+    }
 
-        return 0;
+    public static void drop(int[] currentWait, int t) {
+        for (int i = 0; i < currentWait.length; i++) {
+            if (currentWait[i] != 0) {
+                currentWait[i] = currentWait[i] - t > 0 ? currentWait[i] - t : 0;
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+        int[] arr = {1,1,2,2};
+        System.out.println(askForCoolingTime(arr, 2));
     }
 }
